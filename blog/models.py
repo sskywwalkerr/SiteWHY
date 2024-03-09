70c7ab5c2e6ec4558660ccb79755ae815d103cc7
sskywwalkerr <radmir_rysaev1@mail.ru>
2024-03-09 19:30:36 +0500

    9

--- blog/models.py
+++ blog/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 
 class Post(models.Model):
-    title = models.CharField(max_length=70, verbose_name='Название')
+    title = models.CharField(max_length=200, verbose_name='Название')
     description = models.TextField(verbose_name='Описание')
     image = models.ImageField(upload_to='images', verbose_name='Изображение')
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    price = models.DecimalField(max_digits=50, decimal_places=2, verbose_name='Цена')
 
 
     TYPE = [
-        ('man', 'мужчинам'),
-        ('girl', 'женщинам'),
-        ('child', 'детям'),
+        ('nike', 'Nike'),
+        ('jordan', 'Jordan'),
+        ('new balance', 'New balance'),
+        ('adidas', 'Adidas')
     ]
-    type = models.CharField(choices=TYPE, max_length=5, default='man', verbose_name='тип')
+    type = models.CharField(choices=TYPE, max_length=50, default='nike', verbose_name='тип')
 
     def __str__(self):
         return self.title
-
